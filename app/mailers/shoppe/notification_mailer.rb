86588200be3dc612b6bb8d472ea2a7ecdb1999bc
David Nicol <david@sidstan.com>
2017-03-16 23:33:30 +0000

    Notify users via Retailer association if it exists

--- app/mailers/shoppe/notification_mailer.rb
+++ app/mailers/shoppe/notification_mailer.rb
@@ -3,9 +3,17 @@ module Shoppe
   
     def order_received(order)
       @order = order
-      staff = Shoppe::User.all.map(&:email_address)
+
+      staff = []
+      Shoppe::Retailer.joins(:countries).where(shoppe_countries: {id: order.delivery_country_id}).map(&:users).each do |users|
+        staff << users.map(&:email_address)
+      end
+      staff = staff.uniq
+
+      # Fallback on notifying all users
+      staff = Shoppe::User.all.map(&:email_address) if staff.empty?
+
       mail from: Shoppe.settings.outbound_email_address, to: staff, subject: "New Order Received"
     end
-
   end
 end
