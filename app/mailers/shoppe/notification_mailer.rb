6375173ec93160eb15a84b04db2cf8d3696bced3
David Nicol <david@sidstan.com>
2017-03-17 22:07:17 +0000

    Fix gathering retailer email addresses

--- app/mailers/shoppe/notification_mailer.rb
+++ app/mailers/shoppe/notification_mailer.rb
@@ -5,15 +5,15 @@ module Shoppe
       @order = order
 
       staff = []
-      Shoppe::Retailer.joins(:countries).where(shoppe_countries: {id: order.delivery_country_id}).map(&:users).each do |users|
-        staff << users.map(&:email_address)
+      Shoppe::Retailer.joins(:countries).where(shoppe_countries: {id: order.delivery_country_id}).each do |retailer|
+        staff << retailer.users.map(&:email_address)
       end
-      staff = staff.uniq
+      staff = staff.flatten.uniq
 
-      # Fallback on notifying all users
-      staff = Shoppe::User.all.map(&:email_address) if staff.empty?
+      # Fallback on notifying all users not attached to any retailer (effectively superusers)
+      staff = Shoppe::User.includes(:retailers).where(shoppe_retailers: {id: nil}).map(&:email_address) if staff.empty?
 
-      mail from: Shoppe.settings.outbound_email_address, to: staff, subject: "New Order Received"
+      mail from: Shoppe.settings.outbound_email_address, to: staff, subject: 'New Order Received' unless staff.empty?
     end
   end
 end
