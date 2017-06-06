d0d0945c085ac63543a042e27707da9a3e89a00c
David Nicol <david@sidstan.com>
2017-06-06 23:18:22 +0100

    Add settings for superuser notification

--- lib/shoppe/notification.rb
+++ lib/shoppe/notification.rb
@@ -5,6 +5,9 @@ module Shoppe
   module Notification
     
     def self.setup
+      # Set the configuration which we would like
+      Shoppe.add_settings_group :notifications, [:include_superusers]
+
       Shoppe::Order.before_confirmation do
         Shoppe::NotificationMailer.order_received(self).deliver_now
       end
