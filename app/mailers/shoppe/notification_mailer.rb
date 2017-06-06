75b4bbde5db9e69de70bfa42c29c3bd3921a7480
David Nicol <david@sidstan.com>
2017-06-06 23:17:24 +0100

    Add option to include superusers (users not related with retailers) on all notifications

--- app/mailers/shoppe/notification_mailer.rb
+++ app/mailers/shoppe/notification_mailer.rb
@@ -10,8 +10,10 @@ module Shoppe
       end
       staff = staff.flatten.uniq
 
-      # Fallback on notifying all users not attached to any retailer (effectively superusers)
-      staff = Shoppe::User.includes(:retailers).where(shoppe_retailers: {id: nil}).map(&:email_address) if staff.empty?
+      # Fallback on notifying all users not attached to any retailer (effectively superusers), or if asked to include them
+      if Shoppe.settings.include_superusers.try(:downcase) == 'yes' || staff.empty?
+        staff << Shoppe::User.includes(:retailers).where(shoppe_retailers: {id: nil}).map(&:email_address)
+      end
 
       mail from: Shoppe.settings.outbound_email_address, to: staff, subject: 'New Order Received' unless staff.empty?
     end
